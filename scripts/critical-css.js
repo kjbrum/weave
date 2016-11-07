a26832bcfad1d38f979b8f2f79fcbe499547e973
Kyle Brumm <kjbrumm@gmail.com>
2016-11-07 15:34:13 -0600

    Update critical-css script variable

--- scripts/critical-css.js
+++ scripts/critical-css.js
@@ -5,12 +5,13 @@ const critical = require('critical');
 
 // Variables
 const distDir = 'dist/';
+const criticalFilePath = distDir + 'css/critical.css';
 
 // Generate our critical CSS file
 critical.generate({
     base: distDir,
     src: 'index.html',
-    dest: distDir + 'css/critical.css',
+    dest: criticalFilePath,
     minify: true,
     width: 1200,
     height: 900
